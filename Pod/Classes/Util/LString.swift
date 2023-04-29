4886a47d39b5dba30af5d5e495daacf91c509aca
krgen <krgencom@gmail.com>
2023-04-30 08:41:24 +0900

    Update to version 0.1.16

--- Pod/Classes/Util/LString.swift
+++ Pod/Classes/Util/LString.swift
@@ -1,10 +1,8 @@
-//
-//  LString.swift
-//  Weerket
-//
-//  Created by Krgencom
-//
-
+/**
+ RxUtil 라이브러리는 RxSwift를 이용한 유틸리티 함수를 제공합니다.
+ 
+ 이 라이브러리는 RxSwift를 사용하는 앱에서 사용하기 쉬운 다양한 유틸리티 함수를 제공합니다.
+ */
 import Foundation
 
 func LString(_ key: String, _ : String = "") -> String {
